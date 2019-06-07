044600faeddabf6e70f70a77b167561e90682747
ravikiran1027 <ravikiran1027@gmail.com>
2019-06-07 20:37:29 +0530

    first commit

---
+++ src/test/java/mvnGroupID1/testTest.java
@@ -0,0 +1,15 @@
+package mvnGroupID1;
+
+import org.testng.annotations.Test;
+
+public class testTest {
+	@Test
+	public void testcase1() {
+		System.out.println("1111111111111111111111111");
+	}
+	
+	@Test
+	public void testcase2() {
+		System.out.println("2222222222222222222222222222222222");
+	}
+}
